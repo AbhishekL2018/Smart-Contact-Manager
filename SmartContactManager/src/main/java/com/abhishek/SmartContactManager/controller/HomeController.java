8a0f88fc9bd09103ae4f25f4fe0e0929fba6924c
AbhishekL2018 <abhishekkar647@gmail.com>
2020-10-14 01:51:46 +0530

    3rd commit: Home page setup

--- SmartContactManager/src/main/java/com/abhishek/SmartContactManager/controller/HomeController.java
+++ SmartContactManager/src/main/java/com/abhishek/SmartContactManager/controller/HomeController.java
@@ -1,17 +1,23 @@
 package com.abhishek.SmartContactManager.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/smart-contact-manager")
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
-    public String showHomePage(){
-        return "welcome to my application";
+    public String showHomePage(Model model){
+        model.addAttribute("title","Home-Smart Contact Manager");
+        return "home";
+    }
+
+    @GetMapping("/about")
+    public String showAboutPage(Model model){
+        model.addAttribute("title","About-Smart Contact Manager");
+        return "about";
     }
 }
